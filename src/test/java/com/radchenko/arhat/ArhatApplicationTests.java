5942a109dae57e304d95bea0f227f58612c87685
vradchenko <vit.radcheko@gmail.com>
2019-10-07 17:51:48 +0300

    init commit

---
+++ src/test/java/com/radchenko/arhat/ArhatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radchenko.arhat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArhatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
