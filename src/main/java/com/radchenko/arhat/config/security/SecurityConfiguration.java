af46399bf6592a92fe64a61432abdfe3ada6923f
vradchenko <vit.radcheko@gmail.com>
2019-10-08 18:29:57 +0300

    add session management

--- src/main/java/com/radchenko/arhat/config/security/SecurityConfiguration.java
+++ src/main/java/com/radchenko/arhat/config/security/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -38,6 +39,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.headers().frameOptions().disable();//make h2-console workable
         http.csrf().disable();
+
+        //do not create spring session
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
