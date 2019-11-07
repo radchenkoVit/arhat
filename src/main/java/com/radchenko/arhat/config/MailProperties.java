6e491a1bc5d5d775126b72616169379bdf953647
vradchenko <vit.radcheko@gmail.com>
2019-11-07 11:37:36 +0200

    add more backend validation for properties add validation backend validation for login request

--- src/main/java/com/radchenko/arhat/config/MailProperties.java
+++ src/main/java/com/radchenko/arhat/config/MailProperties.java
@@ -2,21 +2,28 @@ package com.radchenko.arhat.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
-@Configuration
+@ConfigurationProperties
 @PropertySource("classpath:application.properties")
+@Validated
 public class MailProperties {
 
     @Value("${spring.mail.username}")
+    @NotBlank
     private String username;
 
     @Value("${domain.name}")
+    @NotBlank
     private String domain;
 
     @Value("${server.port}")
+    @NotBlank
     public String port;
 
     public String generateActivationCode(String code) {
--- src/main/java/com/radchenko/arhat/web/contoller/user/model/LoginRequest.java
+++ src/main/java/com/radchenko/arhat/web/contoller/user/model/LoginRequest.java
@@ -1,7 +1,16 @@
 package com.radchenko.arhat.web.contoller.user.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class LoginRequest {
+    @Email(message = "Email address should be valid")
+    @NotNull
     private String email;
+
+    @Size(min = 2, max = 30, message = "Password must be between 6 and 30 characters")
+    @NotNull
     private String password;
 
     public LoginRequest() {}
